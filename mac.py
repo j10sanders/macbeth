ac0d2f979930a192ba9ed8717f19affad513409b
j10sanders <jonsandersss@gmail.com>
2017-07-09 12:23:04 -0400

    sort and better naming convention

--- mac.py
+++ mac.py
@@ -4,7 +4,7 @@ import xml.etree.ElementTree as ET
 response = requests.get('http://www.ibiblio.org/xml/examples/shakespeare/macbeth.xml')
 tree = ET.fromstring(response.content)
 
-def speeches(act, values):
+def speeches(act, result):
     speech = act.findall('SCENE/SPEECH')
     for x in speech:
         speakers = x.findall('SPEAKER')
@@ -12,32 +12,32 @@ def speeches(act, values):
         for speaker in speakers:
             if speaker.text == 'ALL':
                 pass
-            elif speaker.text.title() in values:
-                values[speaker.text.title()] += len(lines)
+            elif speaker.text.title() in result:
+                result[speaker.text.title()] += len(lines)
             else:
-                values[speaker.text.title()] = len(lines)
-    return values
+                result[speaker.text.title()] = len(lines)
+    return result
 
 
 def acts(tree):
-    values = {}
+    result = {}
     for child in tree:
         if child.tag == "ACT": 
-            speeches(child, values)
-    return sorted(values.items(), key=lambda character: character[1], reverse=True)
+            speeches(child, result)
+    return sorted(result.items(), key=lambda character: character[1], reverse=True)
 
 
 def choose():
     url = input("Enter the play's url (leave blank for Macbeth): ")
     if len(url) > 0:
-            pass
+        pass
     else:
         url = 'http://www.ibiblio.org/xml/examples/shakespeare/macbeth.xml'
 
     response = requests.get(url)
     tree = ET.fromstring(response.content)
     for key, value in acts(tree):
-        print("%s had %s" % (key, value))
+        print("%s has %s" % (key, value))
 
 
 if __name__ == '__main__':
